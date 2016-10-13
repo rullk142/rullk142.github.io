6821b1b2f0f549ef8451f42abe4dedb184065c9e
Kenisha Rullan <rullk142@newschool.edu>
2016-10-13 18:16:35 +0000

    13 Oct 16

---
+++ jsintro/assets/scripts.js
@@ -0,0 +1,12 @@
+var age = 17;
+var parentsPresent = false;
+
+if (age >= 18 || parentsPresent == true) {
+    console.log("Come on in!");
+} else {
+    if (age == 17) {
+        console.log("Come back next year");
+    } else {
+        console.log("Sorry buddy");
+    }
+}
