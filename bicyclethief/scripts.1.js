86ac1850bd2d564072673a8e796e1c71f64c85d7
Kenisha Rullan <rullk142@newschool.edu>
2016-10-20 16:49:38 +0000

    homepage updated

---
+++ bicyclethief/scripts.1.js
@@ -0,0 +1,10 @@
+var music = true;
+var genreJazz = true;
+
+if (music == rap || genre == jazz) {
+    console.log("Listen to this artist");
+} else {
+    if (music < rap) {
+        console.log("You might not like him, but listen to him anyway...");
+    }
+}
