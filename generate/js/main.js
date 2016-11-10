ea2214213cf3ed60d84dafe5c441c3bd86f8ab71
Kenisha Rullan <rullk142@newschool.edu>
2016-11-10 05:41:07 +0000

    hope

--- generate/js/main.js
+++ generate/js/main.js
@@ -1,45 +1,137 @@
-$("body").css("background-color", "pink");
+$("body").css("background-color","white");
 
 for (var dots = 0; dots < 100; dots++) {
-    $(".wrapper").append("<div class='circle'></div>")
+    $(".wrapper").append("<div class='triangle'></div>");
+}
+
+for (var dots2 = 0; dots2 < 100; dots2++) {
+    $(".wrapper").append("<div class='triangle2'></div>");
 }
 
 var counter = 1;
-var cats = ["img/cat1.png", "img/cat2.jpeg", "img/cat3.jpeg"];
-$("window").resize(function() {
-    $(".circle").each(function() {
-            console.log(counter);
-        $(this).html(counter);
-        $(this).css("opacity", (counter/100));
-        $(this).css("height", (counter+100));
-        $(this).css("width", (counter+100));
-        $(this).css("font-size", (counter));
-            $(this).css("line-height", ("100" + counter + "px"));
-        $(this).css("transform", "rotate("+ (counter/100)*360 + "deg)");
-    // var winWidth = $(window).width();
-    // var winHeight = $(window).height();
-    // var newTop = Math.floor(Math.random()*winHeight);
-    // var newLeft = Math.floor(Math.random()*winWidth);
+var pics = ["img/blm.png","img/disability.png","img/immigration.png","img/lgbt.png","img/muslims.png"];
+
+$(".triangle").each(function() {
+    console.log(counter);
+    $(this).css("opacity",(counter/100));
+    $(this).css("transform","translate(-50%, -50%) rotate("+ (counter/100)*360 + "deg)");
+    var winWidth = $(window).width();
+    var winHeight = $(window).height();
+    var newTop = Math.floor(Math.random()*winHeight);
+    var newLeft = Math.floor(Math.random()*winWidth);
+    $(this).css("top", newTop);
+    $(this).css("left", newLeft);
+    counter++;
     
-        $(this).css("top", Math.floor(Math.random()*$(window).height()));
-        $(this).css("left", Math.floor(Math.random()*$(window).width()));
-        counter++;
-        //this code runs for each instance of circle
+    //this code runs for each instance of triangle
+    
+});
+
+var counter2 = 1;
+
+$(".triangle2").each(function() {
+    console.log(counter2);
+    $(this).css("opacity",(counter2/100));
+    $(this).css("transform","translate(-50%, -50%) rotate("+ (counter2/100)*360 + "deg)");
+    var winWidth = $(window).width();
+    var winHeight = $(window).height();
+    var newTop = Math.floor(Math.random()*winHeight);
+    var newLeft = Math.floor(Math.random()*winWidth);
+    $(this).css("top", newTop);
+    $(this).css("left", newLeft);
+    counter2++;
+    
+    //this code runs for each instance of triangle2
+    
+});
+
+var fist = false;
+
+$(".fist").click(function() {
+    if (fist == false) {
+        var newPic = pics[Math.floor(Math.random()*pics.length)];
+        $("body").append('<img src="' + newPic + '" class="pics">');
+        fist = true
+        $(".triangle").each(function() {
+    console.log(counter);
+    $(this).css("opacity",(counter/100));
+    $(this).css("transform","translate(-50%, -50%) rotate("+ (counter/100)*360 + "deg)");
+    var winWidth = $(window).width();
+    var winHeight = $(window).height();
+    var newTop = Math.floor(Math.random()*winHeight);
+    var newLeft = Math.floor(Math.random()*winWidth);
+    $(this).css("top", newTop);
+    $(this).css("left", newLeft);
+    counter++;
+    
+    //this code runs for each instance of triangle
+    
+});
+
+var counter2 = 1;
+
+$(".triangle2").each(function() {
+    console.log(counter2);
+    $(this).css("opacity",(counter2/100));
+    $(this).css("transform","translate(-50%, -50%) rotate("+ (counter2/100)*360 + "deg)");
+    var winWidth = $(window).width();
+    var winHeight = $(window).height();
+    var newTop = Math.floor(Math.random()*winHeight);
+    var newLeft = Math.floor(Math.random()*winWidth);
+    $(this).css("top", newTop);
+    $(this).css("left", newLeft);
+    counter2++;
+    
+    //this code runs for each instance of triangle2
+    
+});
+    } else {
+        var newPic = pics[Math.floor(Math.random()*pics.length)];
+        $("body").append('<img src="' + newPic + '" class="pics">');
+        $(".triangle").each(function() {
+    console.log(counter);
+    $(this).css("opacity",(counter/100));
+    $(this).css("transform","translate(-50%, -50%) rotate("+ (counter/100)*360 + "deg)");
+    var winWidth = $(window).width();
+    var winHeight = $(window).height();
+    var newTop = Math.floor(Math.random()*winHeight);
+    var newLeft = Math.floor(Math.random()*winWidth);
+    $(this).css("top", newTop);
+    $(this).css("left", newLeft);
+    counter++;
+    
+    //this code runs for each instance of triangle
+    
+});
+
+var counter2 = 1;
+
+$(".triangle2").each(function() {
+    console.log(counter2);
+    $(this).css("opacity",(counter2/100));
+    $(this).css("transform","translate(-50%, -50%) rotate("+ (counter2/100)*360 + "deg)");
+    var winWidth = $(window).width();
+    var winHeight = $(window).height();
+    var newTop = Math.floor(Math.random()*winHeight);
+    var newLeft = Math.floor(Math.random()*winWidth);
+    $(this).css("top", newTop);
+    $(this).css("left", newLeft);
+    counter2++;
+    
+    //this code runs for each instance of triangle2
+    
+});
+        fist = false
+    }
+});
+
+$(window).resize(function() {
+    $(".triangle").each(function() {
+        var winWidth = $(window).width();
+        var winHeight = $(window).height();
+        var newTop = Math.floor(Math.random()*winHeight);
+        var newLeft = Math.floor(Math.random()*winWidth);
+        $(this).css("top", newTop);
+        $(this).css("left", newLeft);
     });
-}
-
-//"this" approaching each circle individually before it moves on to the next
-//math.floor is most common
-
-$(".circle").click(function() {
-    var newCat = cats[Math.floor(Math.random()*cats.length)]
-    $("body").append('<img src="' + newCat + '"newCat++"')
-})
-
-
-// window resize occurs whenever window page changes
-
-$(window).scroll(function()) {
-    console.log($(window).scrollTop())
-    $(this).css("opacity", (window/100));
-}
+});
