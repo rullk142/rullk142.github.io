313ea506ee0283ef068369725295c23e7f068f26
rullk142 <rullk142@newschool.edu>
2017-02-08 11:41:13 -0500

    Started adding storyline

---
+++ 1textadventure/script.js
@@ -0,0 +1,11 @@
+console.log("it's printing")
+
+
+$("#start").click(function() {
+        console.log("congrats, you clicked it")
+        $("#begin").show();
+        $("#yesthat").show();
+            document.getElementById("yesthat").style.transtionDelay = "5s"
+        // $("#gasp").show('slow');
+        // $("#lost").show('slow');
+});
