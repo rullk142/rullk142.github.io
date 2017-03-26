bae1754014daa917d157853544d47320e1d21ebe
rullk142 <rullk142@newschool.edu>
2017-03-26 00:46:36 -0400

    update

--- io/script.js
+++ io/script.js
@@ -2,7 +2,10 @@ window.addEventListener("keydown", function(e) {
     console.log(e);
     
     var length = e.target.textLength/100;
-    document.body.style.background = "rgba(100%, 1.2%, 24.3%, " + length + ")";
+    document.body.style.background = "rgba(100%, 21.2%, 38.6%, " + length + ")";
 // increase opacity of background color when keys are pressed
 // increase opacity of clouds when keys are pressed
 });
+
+//Is there an event listener for time? After a certain amount of time a user does
+//	not interact, background opacity returns to 0 and clouds disappear?
