02ae5d7deba0e78212816db4c45859fe9c8b2ad2
rullk142 <rullk142@newschool.edu>
2017-03-28 21:40:40 -0400

    Workday in class
    Added clouds, added opacity to bg clouds when keyDown, textarea event
    listener for minus opacity in relation to time and not textLength

--- io/script.js
+++ io/script.js
@@ -1,11 +1,25 @@
+var opacity = 0;
+
 window.addEventListener("keydown", function(e) {
     console.log(e);
-    
-    var length = e.target.textLength/100;
-    document.body.style.background = "rgba(100%, 21.2%, 38.6%, " + length + ")";
+    if (e.key != "Backspace") {
+    	//var length = e.target.textLength/100;
+    	opacity = opacity + 0.01;
+    	document.body.style.background = "rgba(100%, 21.2%, 38.6%, " + opacity + ")";
+    	document.body.children.bgclouds.style.opacity = opacity + 0.2;
+	} 
 // increase opacity of background color when keys are pressed
 // increase opacity of clouds when keys are pressed
 });
 
 //Is there an event listener for time? After a certain amount of time a user does
 //	not interact, background opacity returns to 0 and clouds disappear?
+
+setInterval(function(){
+	opacity = opacity - 0.01;
+ 	document.body.style.background = "rgba(100%, 21.2%, 38.6%, " + opacity + ")";
+} ,1000)
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/indexOf
+// https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setInterval
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators
