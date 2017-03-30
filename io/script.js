87384a18d672ca0ddcc17ba07553bce2a4a22783
rullk142 <rullk142@newschool.edu>
2017-03-30 19:17:59 -0400

    Adding audio
    Want to update autoplay to false

--- io/script.js
+++ io/script.js
@@ -4,18 +4,19 @@ window.addEventListener("keydown", function(e) {
     console.log(e);
     if (e.key != "Backspace") {
     	//var length = e.target.textLength/100;
-    	opacity = opacity + 0.005;
+    	opacity = opacity + 0.0025;
     	document.body.style.background = "rgba(100%, 21.2%, 38.6%, " + opacity + ")";
-    	document.body.children.bgclouds.style.opacity = opacity + 0.1;
+    	document.body.children.bgclouds.style.opacity = opacity + 0.05;
 	} 
 });
 
+//change keyup to keydown when finished testing eventListener
 
 setInterval(function(){
 	opacity = opacity - 0.005;
 		document.body.style.background = "rgba(100%, 21.2%, 38.6%, " + opacity + ")"; 	
- 	if ((document.body.children.bgclouds.style.opacity) >= 0.005) {
- 		document.body.children.bgclouds.style.opacity = opacity - 0.005;
+ 	if ((document.body.children.bgclouds.style.opacity) >= 0.0025) {
+ 		document.body.children.bgclouds.style.opacity = opacity - 0.003;
  	}
 } ,1000)
 
@@ -28,3 +29,4 @@ setInterval(function(){
 // change colors to blue
 // add border gradient?
 // string inputs for certain words?
+// add music - Aurarian Dance - Nujabes
