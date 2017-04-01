64440acab02f555404df3869a5b4690ed7bd932e
rullk142 <rullk142@newschool.edu>
2017-04-01 13:35:53 -0400

    Volume
    Volume error no longer goes negative

--- io/script.js
+++ io/script.js
@@ -6,8 +6,9 @@ song.volume = 0.0;
 
 window.addEventListener("keydown", function(e) {
     console.log(e);
-    song.volume = song.volume + 0.005;
-
+    if (song.volume <= 0.65) {
+        song.volume = song.volume + 0.005;
+    } 
     if (e.key != "Backspace" && opacity <= 1.0) {
     	opacity = opacity + 0.01;
     	document.body.style.background = "rgba(0,128,255, " + opacity + ")";
@@ -22,16 +23,39 @@ window.addEventListener("keydown", function(e) {
 
 setInterval(function(){
 	opacity = opacity - 0.02;
-    song.volume = song.volume - 0.02;
-	document.body.style.background = "rgba(0,128,255, " + opacity + ")"; 	
+    
+    // if the current volume - 0.02 < 0
+    //     set the volume to 0
+    // otherwise
+
+    //song.volume = Math.max(0, song.volume - 0.02);
+
+    if (song.volume - 0.02 < 0) {
+        song.volume = 0;
+    } else {
+        song.volume = song.volume - 0.02;
+    }
+	
+
+    document.body.style.background = "rgba(0,128,255, " + opacity + ")"; 	
  	if ((document.body.children.bgclouds.style.opacity) >= 0.01) {
  		document.body.children.bgclouds.style.opacity = opacity - 0.01;
  	}
  	if ((document.body.children.frontclouds.style.opacity) >= 0.01) {
  		document.body.children.frontclouds.style.opacity = opacity - 0.01;
  	}
- 	console.log(song.volume);
+        if ((document.body.children.bgclouds.style.opacity) <= 0.1) {
+            document.body.children.bgclouds.style.opacity = 0;
+        }
+        if ((document.body.children.frontclouds.style.opacity) <= 0.1) {
+            document.body.children.frontclouds.style.opacity = 0;
+        }
+        // if (document.body.style.background <= "rgba(0,128,255, " + 0.1 + ")") {
+        //     document.body.style.background == "rgba(0,128,255, 0)";
+        // }
+    console.log(song.volume);
 } ,2000)
+
 
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/indexOf
@@ -39,5 +63,5 @@ setInterval(function(){
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators
 
 // TO DO
-// add border gradient?
-// string inputs for certain words?
+// How to make volume not go over half
+// Negative number issue
