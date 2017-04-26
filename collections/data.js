5c4b5f4c72dbee06eca8ac839b243d70a188b587
rullk142 <rullk142@newschool.edu>
2017-04-26 09:48:14 -0400

    Added data

--- collections/data.js
+++ collections/data.js
@@ -1,14 +1,146 @@
-var collection = [
+var stickers = [
 	{
-		"date": "4/5",
+		"date": "4/11",
+		"title": "Dazed",
+		"surface": "My Fridge",
+		"location": "My Apartment",
+		"daynumber": 1,
+		"type": "words",
+		"color": ["pink", "purple", "blue"]
+	},
+	{
+		"date": "4/12",
+		"title": "Treasure",
+		"surface": "Electric Pole",
+		"location": "Himrod St, Brooklyn, NY 11237",
 		"daynumber": 2,
-		"type": "illustration",
-		"colors": ["blue","red"]
+		"type": ["words","graffii"],
+		"color": ["pink", "purple", "blue","yellow","green","black"]
 	},
 	{
-		"type": "words"
+		"date": "4/13",
+		"title": "Frank",
+		"surface": "News Box",
+		"location": "33 W 81st St, New York, NY 10024",
+		"daynumber": 3,
+		"type": ["words","illustration"],
+		"color": ["white","black"]
 	},
-	{}
-
-
+	{
+		"date": "4/14",
+		"title": "Skull",
+		"surface": "Electric Pole",
+		"location": "114 Wyckoff Ave, Brooklyn, NY 11237",
+		"daynumber": 4,
+		"type": "illustration",
+		"color": ["white","black"]
+	},
+	{
+		"date": "4/15",
+		"title": "Feminist",
+		"surface": "Glass Wall",
+		"location": "LL / 66 5th Ave, New York, NY 10011",
+		"daynumber": 5,
+		"type": "words",
+		"color": ["white","pink"]
+	},
+	{
+		"date": "4/16",
+		"title": "DM",
+		"surface": "Train Sign",
+		"location": "14 Street / 6 Av, New York, NY 10011",
+		"daynumber": 6,
+		"type": "words",
+		"color": ["blue","green"]
+	},
+	{
+		"date": "4/17",
+		"title": "Zynoz",
+		"surface": "Train Sign",
+		"location": "14 Street / 6 Av, New York, NY 10011",
+		"daynumber": 7,
+		"type": ["graffiti","words"],
+		"color": ["black","white"]
+	},
+	{
+		"date": "4/18",
+		"title": "WANG",
+		"surface": "Subway Pillar",
+		"location": "14 Street / 6 Av, New York, NY 10011",
+		"daynumber": 8,
+		"type": "words",
+		"color": ["black","white"]
+	},
+	{
+		"date": "4/19",
+		"title": "S",
+		"surface": "Subway Pillar",
+		"location": "Myrtle Av, Brooklyn, NY 11206",
+		"daynumber": 9,
+		"type": "words",
+		"color": ["black","white"]
+	},
+	{
+		"date": "4/20",
+		"title": "Bines",
+		"surface": "Postal Relay Box",
+		"location": "E 13th St, New York, NY 10003",
+		"daynumber": 10,
+		"type": ["illustration","graffiti"],
+		"color": ["black","white"]
+	},
+	{
+		"date": "4/21",
+		"title": "Wolf",
+		"surface": "Locker",
+		"location": "10th Floor / 66 5th Ave, New York, NY 10011",
+		"daynumber": 11,
+		"type": "illustration",
+		"color": ["green","red","yellow"]
+	},
+	{
+		"date": "4/22",
+		"title": "Faust",
+		"surface": "Postal Relay Box",
+		"location": "E 13th St, New York, NY 10003",
+		"daynumber": 12,
+		"type": ["graffiti","words"],
+		"color": ["black", "white"]
+	},
+	{
+		"date": "4/23",
+		"title": "Rave",
+		"surface": "My Fridge",
+		"location": "My Apartment",
+		"daynumber": 13,
+		"type": ["brand","words"],
+		"color": ["orange"]
+	},
+	{
+		"date": "4/24",
+		"title": "Catfish",
+		"surface": "My Fridge",
+		"location": "My Apartment",
+		"daynumber": 14,
+		"type": ["brand","words","illustration"],
+		"color": ["orange"]
+	},
+	{
+		"date": "4/25",
+		"title": "Jane Mai",
+		"surface": "My Fridge",
+		"location": "My Apartment",
+		"daynumber": 15,
+		"type": ["brand","words","illustration"],
+		"color": ["black","white"]
+	},
+	{
+		"date": "4/26",
+		"title": "indecline",
+		"surface": "My Fridge",
+		"location": "My Apartment",
+		"daynumber": 16,
+		"type": ["brand","words"],
+		"color": ["black","white"]
+	},
 ];
---
+++ inclass/script.js
@@ -0,0 +1,16 @@
+var shirts = [
+{
+	"size": "large",
+	"color": "red",
+	"stripes": 4
+}, {
+	"size": "small",
+	"color": "blue",	
+	"stripes": 3
+}, {
+	"size": "medium",
+	"color": "green",	
+	"stripes": 5
+}
+];
+
