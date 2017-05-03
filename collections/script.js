a116ad67f33b98ee5f01e13b91a0a53f9af5e0b3
rullk142 <rullk142@newschool.edu>
2017-05-03 11:40:16 -0400

    Fixed for loop, adding filters

--- collections/script.js
+++ collections/script.js
@@ -1,7 +1,3 @@
-// $("#seeall").mouseover(function() {
-//         console.log("congrats, you entered it")
-// });
-
 var found = _.where(stickers);
 
 console.log(found);
@@ -13,50 +9,48 @@ var right = document.getElementById("right");
 
 stickers.forEach(function(item) {
 	console.log(item);
-	for (var i = stickers.length - 1; i <= 21; i++) {
-		console.log(stickers[i]);
+	var itembox = document.createElement("div");
+		itembox.classList.add("sticker");
 
-		var itembox = document.createElement("div");
-			itembox.classList.add("sticker");
-			holder.appendChild(itembox);
+		holder.appendChild(itembox);
 
-		var sticker = document.querySelector(".sticker");
+	var left = document.createElement("div");
+		left.classList.add("left");
+		itembox.appendChild(left);
 
-		var left = document.createElement("div");
-			left.classList.add("left");
-			sticker.appendChild(left);
+	var right = document.createElement("div");
+		right.classList.add("right");
+		itembox.appendChild(right);
 
-		var right = document.createElement("div");
-			right.classList.add("right");
-			sticker.appendChild(right);
-	// }
+	var line = document.createElement("div");
+		line.classList.add("line");
+		holder.appendChild(line);
 
-	// stickers.forEach(function() {
-		var img = document.createElement("img");
-			img.src = item.img;
-			left.appendChild(img);
+	var img = document.createElement("img");
+		img.src = item.img;
+		left.appendChild(img);
 
-			var title = document.createElement("h2");
-				title.textContent = item.title;
-				right.appendChild(title);
-			var date = document.createElement("h3");
-				date.textContent = item.date + "/17";
-				right.appendChild(date);
-			var day = document.createElement("p");
-				day.textContent = "Day " + item.daynumber;
-				right.appendChild(day);
-			var location = document.createElement("p");
-				location.textContent = "Location: " + item.location;
-				right.appendChild(location);
-			var surface = document.createElement("p");
-				surface.textContent = "Surface: " + item.surface;
-				right.appendChild(surface);
-			var tags = document.createElement("p");
-				tags.textContent = "Tags: " + item.type + ", " + item.color;
-				right.appendChild(tags);
-		}
-		// adds all sticker info to holder div
-	// });
+		var title = document.createElement("p");
+			title.textContent = item.title;
+			right.appendChild(title);
+			title.style.fontFamily = "Helvetica Rounded Black";
+			title.style.fontSize = "24px";
+		var day = document.createElement("p");
+			day.textContent = "Day " + item.daynumber;
+			right.appendChild(day);
+		var date = document.createElement("p");
+			date.textContent = item.date + "/17";
+			right.appendChild(date);
+		var location = document.createElement("p");
+			location.textContent = "Location: " + item.location;
+			right.appendChild(location);
+		var surface = document.createElement("p");
+			surface.textContent = "Surface: " + item.surface;
+			right.appendChild(surface);
+		var tags = document.createElement("p");
+			tags.textContent = "Tags: " + item.type + ", " + item.color;
+			right.appendChild(tags);
+	// adds all sticker info to holder div
 });
 
 
@@ -65,12 +59,4 @@ stickers.forEach(function(item) {
 	// console.log(color);
 	// console.log(color.join(", "));
 
-
-
-	// 	var title = document.createElement("h4");
-	// 		title.textContent = found[i].title; 
-	// 		itembox.appendChild(title);
-	// 	var date = document.createElement("h5");
-	// 		itembox.textContent = found[i].date;
-	// 		itembox.appendChild(date);
 
