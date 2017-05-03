56fdc01ba5cfd7559b3ca3c842e2b4bc07f0e7ed
rullk142 <rullk142@newschool.edu>
2017-05-02 20:16:42 -0400

    update

--- collections/script.js
+++ collections/script.js
@@ -8,6 +8,8 @@ console.log(found);
 
 var holder = document.getElementById("holder");
 //calls holder div from seeall.html
+var left = document.getElementById("left");
+var right = document.getElementById("right");
 
 stickers.forEach(function(item) {
 	console.log(item);
@@ -16,37 +18,44 @@ stickers.forEach(function(item) {
 	// 	var date = found[i].date;
 
 		var itembox = document.createElement("div");
-			itembox.classList.add("sticker");
+			itembox.classList.add("profile");
 			holder.appendChild(itembox);
 
+			var pic = document.createElement("div");
+				pic.classList.add("pic");
+				left.appendTo(pic);
 
-			var img = document.createElement("img");
+				var img = document.createElement("img");
 				img.src = item.img;
-				itembox.appendChild(img);
-			var title = document.createElement("h2");
-				title.textContent = item.title;
-				itembox.appendChild(title);
-			var date = document.createElement("h3");
-				date.textContent = item.date + "/17";
-				itembox.appendChild(date);
-			var day = document.createElement("p");
-				day.textContent = "Day " + item.daynumber;
-				itembox.appendChild(day);
-			var location = document.createElement("p");
-				location.textContent = "Location: " + item.location;
-				itembox.appendChild(location);
-			var surface = document.createElement("p");
-				surface.textContent = "Surface: " + item.surface;
-				itembox.appendChild(surface);
-			var tags = document.createElement("p");
-				// var color = item.color;
-				// 	color.join(", ");
-				// var type = item.type;
-					// type.join(", ");
-				tags.textContent = "Tags: " + type + ", " + color;
-				itembox.appendChild(tags);
-		//adds all sticker info to holder div
-	// }
+				left.appendChild(img);
+			
+			var deets = document.createElement("div");
+				deets.classList.add("deets");
+				right.appendChild(deets);
+				var title = document.createElement("h2");
+					title.textContent = item.title;
+					deets.appendChild(title);
+				var date = document.createElement("h3");
+					date.textContent = item.date + "/17";
+					deets.appendChild(date);
+				var day = document.createElement("p");
+					day.textContent = "Day " + item.daynumber;
+					deets.appendChild(day);
+				var location = document.createElement("p");
+					location.textContent = "Location: " + item.location;
+					deets.appendChild(location);
+				var surface = document.createElement("p");
+					surface.textContent = "Surface: " + item.surface;
+					deets.appendChild(surface);
+				var tags = document.createElement("p");
+					// var color = item.color;
+					// 	color.join(", ");
+					// var type = item.type;
+						// type.join(", ");
+					tags.textContent = "Tags: " + item.type + ", " + item.color;
+					deets.appendChild(tags);
+			//adds all sticker info to holder div
+		// }
 
 });
 
