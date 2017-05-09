ff9af6cca3d906ca9d301f92dfefd4614709ac94
rullk142 <rullk142@newschool.edu>
2017-05-09 19:06:58 -0400

    update

--- digital/script.js
+++ digital/script.js
@@ -1,14 +1,16 @@
-// var target = $('#fading');
-// var targetHeight = target.outerHeight();
+var target = $('#fading');
+var targetHeight = target.outerHeight();
 
-// $(document).scroll(function(e){
-// 	console.log(targetHeight);
-// 	console.log(window);
-//     var scrollPercent = (targetHeight - window.scrollY) / targetHeight;
-//     if(scrollPercent >= 0.3){
-//         target.css('opacity', (1 - scrollPercent));
-//     }
-//     // if (window.scrollY <= 220000) {
-//     // 	target.css('opacity', ((1 + scrollPercent)*.3));
-//     // }
-// });
+$(document).scroll(function(e){
+	console.log(targetHeight);
+	console.log(window);
+    var scrollPercent = (targetHeight - window.scrollY) / targetHeight;
+    if(scrollPercent >= 0.0){
+    	// var newTarget = target.not($('#page'));
+    	console.log(target.not($('#page')));
+        target.css('opacity', (1 - scrollPercent));
+    }
+    // if (window.scrollY <= 220000) {
+    // 	target.css('opacity', ((1 + scrollPercent)*.3));
+    // }
+});
