dc0729ff98131ea92b8cfd8e1c8e834b9c49f178
rullk142 <rullk142@newschool.edu>
2017-05-09 18:42:52 -0400

    update

---
+++ digital/script.js
@@ -0,0 +1,14 @@
+// var target = $('#fading');
+// var targetHeight = target.outerHeight();
+
+// $(document).scroll(function(e){
+// 	console.log(targetHeight);
+// 	console.log(window);
+//     var scrollPercent = (targetHeight - window.scrollY) / targetHeight;
+//     if(scrollPercent >= 0.3){
+//         target.css('opacity', (1 - scrollPercent));
+//     }
+//     // if (window.scrollY <= 220000) {
+//     // 	target.css('opacity', ((1 + scrollPercent)*.3));
+//     // }
+// });
