96454e44bf649f4d3afe41b9fd4a2e156dc3fac7
rullk142 <rullk142@newschool.edu>
2017-05-12 18:22:26 -0400

    update
    linking links and images

---
+++ script.js
@@ -0,0 +1,146 @@
+console.log("its printing!");
+
+var doge = document.getElementById("doge");
+var neww = document.getElementById("new");
+var cloud = document.getElementById("cloud");
+var film = document.getElementById("film");
+var media = document.getElementById("media");
+var sina = document.getElementById("sina");
+var stick = document.getElementById("stick");
+var ui = document.getElementById("ui");
+var wdis = document.getElementById("wdis");
+var what = document.getElementById("what");
+var wild = document.getElementById("wild");
+var wire = document.getElementById("wire");
+
+$("#doge, #text").on({
+	"mouseover" : function() {
+		doge.src = "thumb/color/doge.jpg";
+		console.log("mouse over!");
+	},
+	"mouseout" : function() {
+		doge.src = "thumb/bw/doge.jpg";
+		console.log("mouse out!");
+	}
+});
+
+$("#new, #ux").on({
+	"mouseover" : function() {
+		neww.src = "thumb/color/new.jpg";
+		console.log("mouse over!");
+	},
+	"mouseout" : function() {
+		neww.src = "thumb/bw/new.jpg";
+		console.log("mouse out!");
+	}
+});
+
+$("#cloud, #io").on({
+	"mouseover" : function() {
+		cloud.src = "thumb/color/cloud.jpg";
+		console.log("mouse over!");
+	},
+	"mouseout" : function() {
+		cloud.src = "thumb/bw/cloud.jpg";
+		console.log("mouse out!");
+	}
+});
+
+$("#film, #home").on({
+	"mouseover" : function() {
+		film.src = "thumb/color/film.jpg";
+		console.log("mouse over!");
+	},
+	"mouseout" : function() {
+		film.src = "thumb/bw/film.jpg";
+		console.log("mouse out!");
+	}
+});
+
+$("#media, #four").on({
+	"mouseover" : function() {
+		media.src = "thumb/color/media.jpg";
+		console.log("mouse over!");
+	},
+	"mouseout" : function() {
+		media.src = "thumb/bw/media.jpg";
+		console.log("mouse out!");
+	}
+});
+
+$("#sina, #three").on({
+	"mouseover" : function() {
+		sina.src = "thumb/color/sina.jpg";
+		console.log("mouse over!");
+	},
+	"mouseout" : function() {
+		sina.src = "thumb/bw/sina.jpg";
+		console.log("mouse out!");
+	}
+});
+
+$("#stick, #collect").on({
+	"mouseover" : function() {
+		stick.src = "thumb/color/stick.jpg";
+		console.log("mouse over!");
+	},
+	"mouseout" : function() {
+		stick.src = "thumb/bw/stick.jpg";
+		console.log("mouse out!");
+	}
+});
+
+$("#ui, #ux").on({
+	"mouseover" : function() {
+		ui.src = "thumb/color/ui.jpg";
+		console.log("mouse over!");
+	},
+	"mouseout" : function() {
+		ui.src = "thumb/bw/ui.jpg";
+		console.log("mouse out!");
+	}
+});
+
+$("#wdis, #see").on({
+	"mouseover" : function() {
+		wdis.src = "thumb/color/wdis.jpg";
+		console.log("mouse over!");
+	},
+	"mouseout" : function() {
+		wdis.src = "thumb/bw/wdis.jpg";
+		console.log("mouse out!");
+	}
+});
+
+$("#what, #two").on({
+	"mouseover" : function() {
+		what.src = "thumb/color/what.jpg";
+		console.log("mouse over!");
+	},
+	"mouseout" : function() {
+		what.src = "thumb/bw/what.jpg";
+		console.log("mouse out!");
+	}
+});
+
+$("#wild, #things").on({
+	"mouseover" : function() {
+		wild.src = "thumb/color/wild.jpg";
+		console.log("mouse over!");
+	},
+	"mouseout" : function() {
+		wild.src = "thumb/bw/wild.jpg";
+		console.log("mouse out!");
+	}
+});
+
+$("#wire, #frame").on({
+	"mouseover" : function() {
+		wild.src = "thumb/color/wire.jpg";
+		console.log("mouse over!");
+	},
+	"mouseout" : function() {
+		wild.src = "thumb/bw/wire.jpg";
+		console.log("mouse out!");
+	}
+});
