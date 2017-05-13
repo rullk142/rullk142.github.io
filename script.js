f167bd5350ac1da2f1dad05ceabbcf401dc44611
rullk142 <rullk142@newschool.edu>
2017-05-12 22:08:26 -0400

    Fixed!
    Connected links to images!

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 console.log("its printing!");
 
+//image ids
 var doge = document.getElementById("doge");
 var neww = document.getElementById("new");
 var cloud = document.getElementById("cloud");
@@ -13,134 +14,148 @@ var what = document.getElementById("what");
 var wild = document.getElementById("wild");
 var wire = document.getElementById("wire");
 
+//link ids
+	// var text = document.getElementById("text");
+	// var ux = document.getElementById("ux");
+	// var io = document.getElementById("io");
+	// var frame = document.getElementById("frame");
+	// var collect = document.getElementById("collect");
+	// var one = document.getElementById("one");
+	// var two = document.getElementById("two");
+	// var three = document.getElementById("three");
+	// var four = document.getElementById("four");
+	// var see = document.getElementById("see");
+	// var home = document.getElementById("home");
+	// var things = document.getElementById("things");
+
 $("#doge, #text").on({
 	"mouseover" : function() {
 		doge.src = "thumb/color/doge.jpg";
-		console.log("mouse over!");
+		$("#text p").css({"color": "white"});
 	},
 	"mouseout" : function() {
 		doge.src = "thumb/bw/doge.jpg";
-		console.log("mouse out!");
+		$("#text p").css("color", "gray");
 	}
 });
 
 $("#new, #ux").on({
 	"mouseover" : function() {
 		neww.src = "thumb/color/new.jpg";
-		console.log("mouse over!");
+		$("#ux p").css({"color": "white"});
 	},
 	"mouseout" : function() {
 		neww.src = "thumb/bw/new.jpg";
-		console.log("mouse out!");
+		$("#ux p").css({"color": "gray"});
 	}
 });
 
 $("#cloud, #io").on({
 	"mouseover" : function() {
 		cloud.src = "thumb/color/cloud.jpg";
-		console.log("mouse over!");
+		$("#io p").css({"color": "white"});
 	},
 	"mouseout" : function() {
 		cloud.src = "thumb/bw/cloud.jpg";
-		console.log("mouse out!");
+		$("#io p").css({"color": "gray"});
 	}
 });
 
 $("#film, #home").on({
 	"mouseover" : function() {
 		film.src = "thumb/color/film.jpg";
-		console.log("mouse over!");
+		$("#home p").css({"color": "white"});
 	},
 	"mouseout" : function() {
 		film.src = "thumb/bw/film.jpg";
-		console.log("mouse out!");
+		$("#home p").css({"color": "gray"});
 	}
 });
 
 $("#media, #four").on({
 	"mouseover" : function() {
 		media.src = "thumb/color/media.jpg";
-		console.log("mouse over!");
+		$("#four p").css({"color": "white"});
 	},
 	"mouseout" : function() {
 		media.src = "thumb/bw/media.jpg";
-		console.log("mouse out!");
+		$("#four p").css({"color": "gray"});
 	}
 });
 
 $("#sina, #three").on({
 	"mouseover" : function() {
 		sina.src = "thumb/color/sina.jpg";
-		console.log("mouse over!");
+		$("#three p").css({"color": "white"});
 	},
 	"mouseout" : function() {
 		sina.src = "thumb/bw/sina.jpg";
-		console.log("mouse out!");
+		$("#three p").css({"color": "gray"});
 	}
 });
 
 $("#stick, #collect").on({
 	"mouseover" : function() {
 		stick.src = "thumb/color/stick.jpg";
-		console.log("mouse over!");
+		$("#collect p").css({"color": "white"});
 	},
 	"mouseout" : function() {
 		stick.src = "thumb/bw/stick.jpg";
-		console.log("mouse out!");
+		$("#collect p").css({"color": "gray"});
 	}
 });
 
 $("#ui, #one").on({
 	"mouseover" : function() {
 		ui.src = "thumb/color/ui.jpg";
-		console.log("mouse over!");
+		$("#one p").css({"color": "white"});
 	},
 	"mouseout" : function() {
 		ui.src = "thumb/bw/ui.jpg";
-		console.log("mouse out!");
+		$("#one p").css({"color": "gray"});
 	}
 });
 
 $("#wdis, #see").on({
 	"mouseover" : function() {
 		wdis.src = "thumb/color/wdis.jpg";
-		console.log("mouse over!");
+		$("#see p").css({"color": "white"});
 	},
 	"mouseout" : function() {
 		wdis.src = "thumb/bw/wdis.jpg";
-		console.log("mouse out!");
+		$("#see p").css({"color": "gray"});
 	}
 });
 
 $("#what, #two").on({
 	"mouseover" : function() {
 		what.src = "thumb/color/what.jpg";
-		console.log("mouse over!");
+		$("#two p").css({"color": "white"});
 	},
 	"mouseout" : function() {
 		what.src = "thumb/bw/what.jpg";
-		console.log("mouse out!");
+		$("#two p").css({"color": "gray"});
 	}
 });
 
 $("#wild, #things").on({
 	"mouseover" : function() {
 		wild.src = "thumb/color/wild.jpg";
-		console.log("mouse over!");
+		$("#things p").css({"color": "white"});
 	},
 	"mouseout" : function() {
 		wild.src = "thumb/bw/wild.jpg";
-		console.log("mouse out!");
+		$("#things p").css({"color": "gray"});
 	}
 });
 
 $("#wire, #frame").on({
 	"mouseover" : function() {
 		wire.src = "thumb/color/wire.jpg";
-		console.log("mouse over!");
+		$("#frame p").css({"color": "white"});
 	},
 	"mouseout" : function() {
 		wire.src = "thumb/bw/wire.jpg";
-		console.log("mouse out!");
+		$("#frame p").css({"color": "gray"});
 	}
 });
